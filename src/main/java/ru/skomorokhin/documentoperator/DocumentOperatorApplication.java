0da51b97dab7950391da8bc4c0e0aee45445861e
ASkomorokhin <anton_mtd1055@mail.ru>
2026-02-19 12:47:39 +0300

    init

---
+++ src/main/java/ru/skomorokhin/documentoperator/DocumentOperatorApplication.java
@@ -0,0 +1,11 @@
+package ru.skomorokhin.documentoperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentOperatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentOperatorApplication.class, args);
+    }
+}
